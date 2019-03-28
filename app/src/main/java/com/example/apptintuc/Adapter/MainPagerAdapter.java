9296889c1b94db42b948b9ffff63242a57d0aa60
ThanhQvanVnnn <phung.thanhquan1996@gmail.com>
2019-03-28 23:16:50 +0700

    xong hiển thị danh mục theo viewPager

--- app/src/main/java/com/example/apptintuc/Adapter/MainPagerAdapter.java
+++ app/src/main/java/com/example/apptintuc/Adapter/MainPagerAdapter.java
@@ -1,10 +1,12 @@
 package com.example.apptintuc.Adapter;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.example.apptintuc.DanhMucTinTucFragment;
 import com.example.apptintuc.Object.DanhMuc;
 
 import java.util.ArrayList;
@@ -12,7 +14,7 @@ import java.util.List;
 
 public class MainPagerAdapter extends FragmentPagerAdapter {
 
-    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<DanhMucTinTucFragment> fragmentList = new ArrayList<>();
     private List<DanhMuc> tabTitles = new ArrayList<>();
 
 
@@ -23,7 +25,11 @@ public class MainPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int i) {
-        return fragmentList.get(i);
+        Bundle bundle = new Bundle();
+        bundle.putInt("idDanhMuc",tabTitles.get(i).getIdDanhMuc());
+        DanhMucTinTucFragment danhMucTinTucFragment =  fragmentList.get(i);
+        danhMucTinTucFragment.setArguments(bundle);
+        return danhMucTinTucFragment;
     }
 
     @Override
@@ -36,7 +42,7 @@ public class MainPagerAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
         return tabTitles.get(position).getTenDanhMuc();
     }
-    public void addFragment(Fragment fragment, DanhMuc danhMucs) {
+    public void addFragment(DanhMucTinTucFragment fragment, DanhMuc danhMucs) {
         fragmentList.add(fragment);
         tabTitles.add(danhMucs);
     }
--- app/src/main/java/com/example/apptintuc/DanhMucTinTucFragment.java
+++ app/src/main/java/com/example/apptintuc/DanhMucTinTucFragment.java
@@ -7,12 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DanhMucTinTucFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.danhmuctintuc_fragment_layout,container,false);
+        TextView textView = view.findViewById(R.id.text);
+        Bundle bundle=getArguments();
+        int id_danhmuc = bundle.getInt("idDanhMuc");
+        textView.setText(id_danhmuc+"");
         return view;
     }
 }
